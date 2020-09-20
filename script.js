2e659e292fc778b44aab58deee964006d55d49e0
Claire Vita <clairej.vita@gmail.com>
2020-09-20 19:43:01 -0400

    added navbar. added modal for highscores.

---
+++ script.js
@@ -0,0 +1,9 @@
+//Timer
+//Local Storage
+//Quiz
+//Dom population with formatted questions and answers
+//Decreases time by 15 seconds on incorrect answer
+//Time stops and is set as your scpre
+//View Highscores
+    //Highscores is a separate page
+//Small notification that lets you know if you were right or wrong. Shows briefly.
