75e7b32744d9c80375cc052a9a370b64c3ff06d0
Claire Vita <clairej.vita@gmail.com>
2020-09-20 23:22:27 -0400

    Added Javascript functionality of the start button, toggling the quiz zone to visible and the start button to invisible.

--- script.js
+++ script.js
@@ -7,3 +7,13 @@
 //View Highscores
     //Highscores is a separate page
 //Small notification that lets you know if you were right or wrong. Shows briefly.
+
+//These identify the start button and the area invisible area that the questions will be placed.
+let startButton = document.getElementById("startButton");
+let quizZone = document.getElementById("quizZone");
+
+//This click event removes the start button and replaces it with the quiz zone.
+startButton.addEventListener("click", function() {
+  startButton.className += " invisible";
+  quizZone.className -= "invisible";
+});
