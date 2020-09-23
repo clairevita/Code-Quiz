14a3366d7fae9c1deaa14d254ecfa21ccc59ff57
Claire Vita <clairej.vita@gmail.com>
2020-09-23 00:44:07 -0400

    Added modal player data visualization. Addfed random question selection. Added player score. Linked quiz system.

--- script.js
+++ script.js
@@ -16,15 +16,43 @@ let quizZone = document.getElementById("quizZone");
 //-----------------------------------------------------------------------------------------------
 //This identifies the quiz timer in the document.
 let quizTimer = document.getElementById("quizTimer");
-let timeRemain = 1
+let timeRemain = 75;
 quizTimer.textContent += timeRemain;
 //This variable simplifies our interaction with the DOM for every second change.
 let changeTimer = parseInt(quizTimer.textContent);
 //Once the user sets this variable to TRUE, the countdown timer will start.
 let timerOn = false;
+//Global timer variable so it can be stopped
+let timerVar;
 //-----------------------------------------------------------------------------------------------
+//This will be the player's inputted answer.
+let selectedAnswer = 0;
+//This is the correct answer.
+let correctAnswer = 0;
+//Whenever a user submits a correct answer, this variable increases.
+let playerScore = 0;
+let scoreDisplay = document.getElementById("playerScore");
+//This is where the new question will be appended.
+let activeQuestion = document.getElementById("questionZone");
+//This counts to see how many questions have been answered.
+let questionCounter = 15;
+//These are the answer buttons where new text will be appended.
+let activeA = document.getElementById("answerA");
+let activeB = document.getElementById("answerB");
+let activeC = document.getElementById("answerC");
+let activeD = document.getElementById("answerD");
+activeA.addEventListener("click", submitA);
+activeB.addEventListener("click", submitB);
+activeC.addEventListener("click", submitC);
+activeD.addEventListener("click", submitD);
+//-----------------------------------------------------------------------------------------------
+//This variable will be the player's score.
+let playerRecording = {score:"", time:"", initials:""};
 //This identifies the modal element where the user can view and submit their score.
-let scoreEntry = document.getElementById("scoreEntry")
+let scoreEntry = document.getElementById("scoreEntry");
+let scoreQuality = document.getElementById("scoreQuality");
+let scoreView = document.getElementById("finalScore");
+let timeView = document.getElementById("finalTime");
 //-----------------------------------------------------------------------------------------------
 
 
@@ -39,7 +67,7 @@ startButton.addEventListener("click", function() {
 //This is the countdown function called by the startButton. Once the time is 0, the text changes to "Time's Up" and shows the results.
 function startTimer() {
   if (timerOn = true){
-     let timerVar = setInterval(countdownTimer, 1000);
+     timerVar = setInterval(countdownTimer, 1000);
       function countdownTimer (){
         timeRemain--;
         quizTimer.textContent = "Time: " + timeRemain;
@@ -56,9 +84,68 @@ function startTimer() {
     }
   }
 
-//When the timer hits zero, this opens up the score display and entry field. The most intuitive way to achieve this is with JQuery.
+//When a player selects an answer, this is the algorithm that checks if it was correct or not.
+function submitA(){
+  selectedAnswer = 1;
+  checkAnswer();
+}
+function submitB(){
+  selectedAnswer = 2;
+  checkAnswer();
+}
+function submitC(){
+  selectedAnswer = 3;
+  checkAnswer();
+}
+function submitD(){
+  selectedAnswer = 4;
+  checkAnswer();
+}
+
+//This checks the answer that they submitted. If it is true it progresses. If it is false it deducts 15 seconds from their time.
+function checkAnswer(){
+  if (selectedAnswer == correctAnswer && questionCounter != 0){
+    playerScore = playerScore + 50;
+    scoreDisplay.textContent = playerScore;
+    correctAnswer = Math.ceil(Math.random()*5);
+    questionCounter = questionCounter - 1;
+
+  } else if (selectedAnswer != correctAnswer && questionCounter != 0){
+    timeRemain = timeRemain - 15;
+    correctAnswer = Math.ceil(Math.random()*5);
+    questionCounter = questionCounter - 1;
+
+  } else if (selectedAnswer == correctAnswer && questionCounter == 0){
+    playerScore = playerScore + 50;
+    timerOn = false;
+    console.log(timerOn);
+    quizTimer.textContent = "Time: " + timeRemain;
+    quizZone.className += " invisible";
+    clearInterval(timerVar);
+    setTimeout(function(){showScore();}, 1000);
+  } else {
+    timerOn = false;
+    console.log(timerOn);
+    quizTimer.textContent = "Time: " + timeRemain;
+    quizZone.className += " invisible";
+    clearInterval(timerVar);
+    setTimeout(function(){showScore();}, 1000);
+  }
+  } 
+
+//This opens up the score display and entry field. The most intuitive way to achieve this is with JQuery.
 function showScore(){
+  if(playerScore>=50){
+    scoreQuality.textContent = "Nice Score!"
+  } else{
+    scoreQuality.textContent = "Don't Give Up"
+  }
+  if (timeRemain < 0){
+    timeRemain = 0;
+  }
   $('#scoreEntry').modal('toggle');
+  scoreView.textContent = playerScore;
+  timeView.textContent = timeRemain;
 }
 
 
